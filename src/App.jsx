b487290ce6fad54c37e4238b310523b28126c495
Wahaj Ali <111431787+Wahaj-Ali@users.noreply.github.com>
2024-06-03 01:46:43 +0500

    Initialized React app using vite and tailwind.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import MultiLayeredParallax from './components/MultiLayeredParallax/MultiLayeredParallax'
+
+function App() {
+
+  return (
+    <>
+      <MultiLayeredParallax />
+      <div className='w-full bg-black'>
+        <div className="max-w-lg space-y-4 mx-auto py-24 text-neutral-300">
+          <p className='p-2 my-4 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis similique molestiae amet aperiam dicta ipsam ut fugit, consequatur, iusto perspiciatis, labore dolores quam recusandae commodi maiores! Unde excepturi fugit dolorum.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim quibusdam accusamus laudantium rerum perspiciatis, molestias necessitatibus iure exercitationem inventore fugit. Commodi atque expedita a accusantium eum, aperiam temporibus aspernatur fugiat.</p>
+
+          <p className='p-2 my-4 text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim quibusdam accusamus laudantium rerum perspiciatis, molestias necessitatibus iure exercitationem inventore fugit. Commodi atque expedita a accusantium eum, aperiam temporibus aspernatur fugiat.</p>
+          <p className='p-2 my-4 text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim quibusdam accusamus laudantium rerum perspiciatis, molestias necessitatibus iure exercitationem inventore fugit. Commodi atque expedita a accusantium eum, aperiam temporibus aspernatur fugiat.</p>
+          <p className='p-2 my-4 text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim quibusdam accusamus laudantium rerum perspiciatis, molestias necessitatibus iure exercitationem inventore fugit. Commodi atque expedita a accusantium eum, aperiam temporibus aspernatur fugiat.</p>
+          <p className='p-2 my-4 text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim quibusdam accusamus laudantium rerum perspiciatis, molestias necessitatibus iure exercitationem inventore fugit. Commodi atque expedita a accusantium eum, aperiam temporibus aspernatur fugiat.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
